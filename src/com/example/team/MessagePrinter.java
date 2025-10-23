dedb5db821ee119f69a5c968a23cf14da97143ce
Dinesh-Kumar-Reddy <50023765+Dinesh-Kumar-Reddy@users.noreply.github.com>
2025-10-23 21:58:29 +0530

    Create MessagePrinter.java

---
+++ src/com/example/team/MessagePrinter.java
@@ -0,0 +1,12 @@
+package com.example.team;
+
+public class MessagePrinter {
+    public void printWelcomeMessage() {
+        System.out.println("Welcome, Team Dinesh & Viresh!");
+        System.out.println("Let's learn Git and GitHub collaboratively!");
+    }
+
+    public void printGoodbyeMessage() {
+        System.out.println("Goodbye! Keep practicing Git daily!");
+    }
+}
